71702771ccff215a1c38372f066d7b17e7d80b21
j4ck41b3m <118809923+j4ck41b3m@users.noreply.github.com>
2022-12-09 10:10:45 +0100

    Add files via upload

--- modelosAR.js
+++ modelosAR.js
@@ -5,11 +5,42 @@ window.onLoad = ()=>
 }
 function cargarModelos()
 {
-	let latitud=37.192274783068676 ;
-	let longitud =-3.6165853004243886;
+	let modelos = [
+		{
+			latitud: 37.192274783068676 ;
+			longitud: -3.6165853004243886;
+			color: 'yellow',
+			escala: 0.1
+		} ,
 
-	var modelo = document.createElement('a-box');
-	modelo,setAttribute('material','color:yellow');
-	modelo.setAttribute('scale','0.5 0.5 0.5');
-	modelo.setAttribute('gps-entity-place','latitude: ${latitud}; longitude: ${longitud};');
-}
+		{
+			latitud: 37.192274783068676 ;
+			longitud: -3.6165853004243886;
+			color: 'red',
+			escala: 0.3
+
+
+		} ,
+
+		{
+			latitud: 37.192274783068676 ;
+			longitud: -3.6165853004243886;
+			color: 'blue',
+			escala: 0.5
+
+		}
+
+	];
+// let latitud=37.192274783068676 ;
+	//let longitud =-3.6165853004243886;
+	for(let i = 0 ; i < modelos.length; i++)
+	{
+		var modelo = document.createElement('a-box');
+	modelo,setAttribute('material','modelos[i].color');
+	modelo.setAttribute('scale','' + modelos[i].escala + modelos[i].escala + modelos[i].escala  );
+	modelo.setAttribute('gps-entity-place','latitude: ${modelos[i].latitud}; longitude: ${modelos[i].longitud};');
+}   
+    let scene = document.querySelector('a-scene');
+    scene.apenChild(model);
+	}
+	
